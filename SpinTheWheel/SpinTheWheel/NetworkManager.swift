13500744ab0d3ed8da1598b8d46565fff7878e82
ahavamorse <ahavamorse@icloud.com>
2021-08-02 14:52:05 -0700

    Created NetworkManager and url variable

---
+++ SpinTheWheel/SpinTheWheel/NetworkManager.swift
@@ -0,0 +1,14 @@
+//
+//  NetworkManager.swift
+//  SpinTheWheel
+//
+//  Created by HapiDani on 8/2/21.
+//  Copyright © 2021 ahavamorse. All rights reserved.
+//
+
+import Foundation
+
+enum NetworkManager {
+    
+    static let url = "http://mockbin.org/bin/dc24c4de-102f-49bf-9c80-9ed52d4ea7f6"
+}
