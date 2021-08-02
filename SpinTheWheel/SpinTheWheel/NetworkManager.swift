e0e72c1483be253d75c5489f8786dbf9d65cd978
ahavamorse <ahavamorse@icloud.com>
2021-08-02 15:01:25 -0700

    Implemented getRewards function for NetworkManager

--- SpinTheWheel/SpinTheWheel/NetworkManager.swift
+++ SpinTheWheel/SpinTheWheel/NetworkManager.swift
@@ -10,5 +10,38 @@ import Foundation
 
 enum NetworkManager {
     
-    static let url = "http://mockbin.org/bin/dc24c4de-102f-49bf-9c80-9ed52d4ea7f6"
+    static let urlString = "http://mockbin.org/bin/dc24c4de-102f-49bf-9c80-9ed52d4ea7f6"
+    
+    func getRewards(completed: @escaping (Result<[Reward], Error>) -> Void) {
+        guard let url = URL(string: NetworkManager.urlString) else {
+            completed(.failure(.invalidUrl))
+            return
+        }
+        
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            if let _ = error {
+                completed(.failure(.invalidRequest))
+                return
+            }
+            
+            guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+                completed(.failure(.invalidResponse))
+                return
+            }
+            
+            guard let data = data else {
+                completed(.failure(.invalidData))
+                return
+            }
+            
+            do {
+                let decoder = JSONDecoder()
+                let rewards = try decoder.decode([Reward].self, from: data)
+                completed(.success(rewards))
+            } catch {
+                completed(.failure(.invalidData))
+            }
+        }
+        task.resume()
+    }
 }
