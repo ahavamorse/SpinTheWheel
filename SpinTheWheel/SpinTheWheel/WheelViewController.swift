15d3777d05373ba38ab6e75f22f3810b90b79238
ahavamorse <ahavamorse@icloud.com>
2021-08-02 14:24:18 -0700

    Created the WheelViewController

---
+++ SpinTheWheel/SpinTheWheel/WheelViewController.swift
@@ -0,0 +1,16 @@
+//
+//  WheelViewController.swift
+//  SpinTheWheel
+//
+//  Created by HapiDani on 8/2/21.
+//  Copyright © 2021 ahavamorse. All rights reserved.
+//
+
+import UIKit
+
+class WheelViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
