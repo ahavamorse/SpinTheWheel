442f143fdd6a17a483ebaeebd964a4803d436367
ahavamorse <ahavamorse@icloud.com>
2021-08-02 13:45:19 -0700

    Created MenuViewController and variables

---
+++ SpinTheWheel/SpinTheWheel/MenuViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MenuViewController.swift
+//  SpinTheWheel
+//
+//  Created by HapiDani on 8/2/21.
+//  Copyright © 2021 ahavamorse. All rights reserved.
+//
+
+import UIKit
+
+class MenuViewController: UIViewController {
+    
+    let titleLabel = UILabel()
+    let spinButton = UIButton()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
