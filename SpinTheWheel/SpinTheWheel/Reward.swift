87b832702e88dccfdf297ac6de9b3a6246bc701d
ahavamorse <ahavamorse@icloud.com>
2021-08-02 15:03:25 -0700

    Created Reward model

---
+++ SpinTheWheel/SpinTheWheel/Reward.swift
@@ -0,0 +1,14 @@
+//
+//  Reward.swift
+//  SpinTheWheel
+//
+//  Created by HapiDani on 8/2/21.
+//  Copyright © 2021 ahavamorse. All rights reserved.
+//
+
+import Foundation
+
+struct Reward {
+    var displayText: String
+    var value: Int
+}
