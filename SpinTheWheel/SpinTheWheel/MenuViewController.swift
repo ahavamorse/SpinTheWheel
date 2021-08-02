0fbabe2f7f9a5e1a311023a3e1d28462dc5dac92
ahavamorse <ahavamorse@icloud.com>
2021-08-02 14:02:08 -0700

    Implemented configureSpinButton for MenuViewController

--- SpinTheWheel/SpinTheWheel/MenuViewController.swift
+++ SpinTheWheel/SpinTheWheel/MenuViewController.swift
@@ -11,21 +11,29 @@ import UIKit
 class MenuViewController: UIViewController {
     
     let titleLabel = UILabel()
-    let spinButton = UIButton()
+    let spinButton = UIButton(frame: .zero)
 
     override func viewDidLoad() {
         super.viewDidLoad()
         configureViewController()
         configureTitleLabel()
+        configureSpinButton()
     }
     
     private func configureViewController() {
-        view.backgroundColor = UIColor(named: "backgroundColor")
+        view.backgroundColor = UIColor(named: "viewBackgroundColor")
     }
     
     private func configureTitleLabel() {
         titleLabel.text = "Rewards Wheel"
         titleLabel.textColor = .white
         titleLabel.font = .preferredFont(forTextStyle: .largeTitle)
+    }
+    
+    private func configureSpinButton() {
+        spinButton.setTitle("Spin the Wheel", for: .normal)
+        spinButton.titleLabel?.font = .preferredFont(forTextStyle: .title1)
+        spinButton.setTitleColor(.label, for: .normal)
+        spinButton.backgroundColor = UIColor(named: "buttonBackgroundColor")
     }
 }
