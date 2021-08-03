3a4da98f457538194b2550ea8afca5f5e2738cfd
ahavamorse <ahavamorse@icloud.com>
2021-08-03 09:40:26 -0700

    Implemented drawing of wheel for WheelView

--- SpinTheWheel/SpinTheWheel/WheelView.swift
+++ SpinTheWheel/SpinTheWheel/WheelView.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class WheelView: UIView {
     
+    var rewards: [Reward]!
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         backgroundColor = UIColor.clear
@@ -19,8 +21,36 @@ class WheelView: UIView {
         fatalError("init(coder:) has not been implemented")
     }
     
+    convenience init(rewards: [Reward]) {
+        self.init(frame: .zero)
+        self.rewards = rewards
+    }
+    
     override func draw(_ rect: CGRect) {
-        // Drawing code
+        if let context = UIGraphicsGetCurrentContext() {
+            
+            let radius = rect.size.width / 2
+            let center = CGPoint(x: radius, y: radius)
+            
+            let numOfSections = CGFloat(rewards.count)
+            let angleIncrement: CGFloat = .pi / (numOfSections / 2)
+            
+            var currentAngle: CGFloat = 0
+
+            let colors = [UIColor.init(named: "wheelLightBackgroundColor")!.cgColor, UIColor.init(named: "wheelDarkBackgroundColor")!.cgColor]
+            
+            for i in 0...rewards.count - 1 {
+                let endAngle = currentAngle + angleIncrement
+                
+                context.move(to: center)
+                context.addArc(center: center, radius: radius, startAngle: currentAngle, endAngle: endAngle, clockwise: false)
+                
+                context.setFillColor(colors[i % 2])
+                context.fillPath()
+                
+                currentAngle = endAngle
+            }
+        }
     }
 
 }
