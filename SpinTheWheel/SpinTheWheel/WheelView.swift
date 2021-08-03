292cefb1447c59dcaba8dc83382cd591867ca39a
ahavamorse <ahavamorse@icloud.com>
2021-08-02 18:44:45 -0700

    Created WheelView and initializers

---
+++ SpinTheWheel/SpinTheWheel/WheelView.swift
@@ -0,0 +1,26 @@
+//
+//  WheelView.swift
+//  SpinTheWheel
+//
+//  Created by HapiDani on 8/2/21.
+//  Copyright © 2021 ahavamorse. All rights reserved.
+//
+
+import UIKit
+
+class WheelView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = UIColor.clear
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+
+}
